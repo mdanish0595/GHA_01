c035bb06cc4db5297033060fc819fc9db194e899
Danish <737615@cognizant.com>
2025-10-02 20:57:41 +0100

    updated terraform code

--- terraform-sg/provider.tf
+++ terraform-sg/provider.tf
@@ -1,13 +1,4 @@
-provider "aws" {
-    region     = "us-east-1"
-    }
-
-/*
 terraform {
   backend "s3" {
-    bucket = "mybucket"
-    key    = "path/to/my/key"
-    region = "us-east-1"
   }
 }
-*/
